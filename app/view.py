120e154b616bfebb4abb139c532ccd77692bc1e0
justus <justus@Notebook>
2015-10-10 11:37:33 +0700

    countdown

--- app/view.py
+++ app/view.py
@@ -1,9 +1,14 @@
 __author__ = 'justus'
 
 from app import app
-from flask import render_template, request, url_for, redirect
+from flask import render_template, request, url_for, redirect, send_from_directory
 
 
 @app.route('/')
 def index():
     return render_template('ouch-pack/index.html')
+
+
+@app.route('/robots.txt')
+def robots():
+    return send_from_directory(app.static_folder, request.path[1:])
