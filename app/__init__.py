b4e7c2fa7b9a929e60e33a31ae9ce042e65b2727
eduna75 <oom.gek@gmail.com>
2016-02-01 23:56:28 +0700

    update requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,9 @@ __author__ = 'justus'
 import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.compress import Compress
 
 app = Flask(__name__, static_path='')
 
-Compress(app)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 db = SQLAlchemy(app)
