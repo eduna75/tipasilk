355f8ce6b19c9dfdf0cda381fe1d4a76f0292a35
eduna75 <oom.gek@gmail.com>
2016-02-01 12:17:42 +0700

    css optimization tryout

--- app/admin/admin.py
+++ app/admin/admin.py
@@ -2,30 +2,24 @@ __author__ = 'justus'
 
 from flask import Blueprint, request, g, redirect, url_for, abort, render_template
 
-admin = Blueprint('admin', __name__, template_folder='templates', url_prefix='/admin')
+admin = Blueprint('admin', __name__, template_folder='admin/templates', url_prefix='/admin')
 
 
 from flask.views import MethodView
 
-from .models import Blog
+from models import Blog
 
 
 class CRUDView(MethodView):
 
-    list_template = 'admin/list_view.html'
-
-    def __init__(self, model, endpoint, list_template=None):
-        self.model = model
-        self.endpoint = endpoint
-        self.path = url_for('.%s' % self.endpoint)
+    def __init__(self, list_template=None):
+        self.list_template = '/admin/listview.html'
         if list_template:
             self.list_template = list_template
 
     def get(self):
-        obj = self.model.query.all()
-        return render_template(list_template, obj=obj, path=self.path)
+        obj = 'this and that'
+        return render_template(self.list_template, obj=obj)
 
 
-view = CRUDView.as_view('blog')
-
-admin.add_url_rule('/blog/', view_func=view, methods=['GET', 'POST'])
+admin.add_url_rule('/blog/', view_func=CRUDView.as_view('blog'), methods=['GET', 'POST'])
