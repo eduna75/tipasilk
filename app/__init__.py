2112fc1574f7da7e784f9521fba12b51510d2de2
eduna75 <oom.gek@gmail.com>
2016-02-02 00:28:38 +0700

    added compression and optimized about us

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,11 @@ __author__ = 'justus'
 import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.compress import Compress
 
 app = Flask(__name__, static_path='')
-
-app.config.from_object(os.environ['APP_CONFIG'])
+Compress(app)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 db = SQLAlchemy(app)
 
