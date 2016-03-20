09ccae209ba5944a86acc63ad39679557c79dc05
eduna75 <oom.gek@gmail.com>
2016-03-20 16:34:02 +0700

    Shopping cart

--- app/store/view.py
+++ app/store/view.py
@@ -17,12 +17,12 @@ def cart():
         product = Products.query.get(request.form['product'])
         if 'cart' in session:
             if not any(product.name in d for d in session['cart']):
-                session['cart'].append({product.name: int(request.form['quantity'])})
+                session['cart'].append({product.name: [int(request.form['quantity']), int(float(request.form['price']))]})
             elif any(product.name in d for d in session['cart']):
                 for d in session['cart']:
-                    d.update((k, int(request.form['quantity'])) for k, v in d.items() if k == product.name)
+                    d.update((k, [int(request.form['quantity']), int(float(request.form['price']))]) for k, v in d.items() if k == product.name)
         else:
-            session['cart'.format(id)] = [{product.name: int(request.form['quantity'])}]
+            session['cart'.format(id)] = [{product.name: [int(request.form['quantity']), int(float(request.form['price']))]}]
         if 'delete' in request.form:
             pass
     return redirect(request.referrer)
