6046d192087f3c3ee409b70ec83ce313564ef965
eduna75 <oom.gek@gmail.com>
2016-03-29 22:16:23 +0700

    Shopping cart

--- app/store/view.py
+++ app/store/view.py
@@ -13,6 +13,7 @@ def index():
 
 @mod.route('/cart', methods=['GET', 'POST'])
 def cart():
+    print session['cart']
     if request.method == 'POST':
         product = Products.query.get(request.form['product'])
         if 'cart' in session:
@@ -23,9 +24,8 @@ def cart():
                                                        product.id]})
             elif any(product.name in d for d in session['cart']):
                 for d in session['cart']:
-                    d.update(
-                        (k, [int(request.form['quantity']), int(float(request.form['price'])), product.product_nr, product.id]) for k, v in d.items() if
-                        k == product.name)
+                    d.update((k, [int(request.form['quantity']), int(float(request.form['price'])), product.product_nr,
+                             product.id]) for k, v in d.items() if k == product.name)
         else:
             session['cart'.format(id)] = [{product.name: [int(request.form['quantity']),
                                                           int(float(request.form['price'])),
